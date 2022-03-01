688c8c1e8d0003af0c9ff4d529d476261e1c057a
user42 <ajuln@hotmail.fr>
2022-03-01 18:43:48 +0100

    fini

--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: jandre <ajuln@hotmail.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 13:47:09 by jandre            #+#    #+#             */
-/*   Updated: 2022/02/28 19:33:19 by jandre           ###   ########.fr       */
+/*   Updated: 2022/03/01 18:40:21 by jandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define VECTOR_HPP
 # include "VectorIterators.hpp"
 # include <memory>
+# include <stdexcept>
 
 
 namespace ft
@@ -97,14 +98,6 @@ namespace ft
 				this->assign(other.begin(), other.end());
 				return (*this);
 			};
-            reference operator[](size_type n)
-			{
-				return (this->_container[n]);
-			};
-			const_reference operator[](size_type n) const
-			{
-				return (this->_container[n]);
-			};
     
         // Iterators
         // Returns an iterator pointing to the first element in the vector.
@@ -178,7 +171,233 @@ namespace ft
 				return (this->_length == 0);
 			};
         // Requests that the vector capacity be at least enough to contain n elements.
-            
+            void reserve(size_type n)
+			{
+                if (n > this->max_size())
+                    throw(std::length_error());
+				if (n > this->_size)
+				{
+					size_type i = -1;
+					pointer	tmp;
+					tmp  = this->_allocator.allocate(n);
+					this->_size = n;
+					while (++i < this->_length)
+						tmp[i] = this->_container[i];
+					this->_allocator.deallocate(this->_container, this->_size);
+					this->_container = tmp;
+				}
+			};
+        // Element access
+        // Returns a reference to the element at position n in the vector container.
+            reference operator[](size_type n)
+			{
+				return (this->_container[n]);
+			};
+        // Returns a constant reference to the element at position n in the vector container.
+			const_reference operator[](size_type n) const
+			{
+				return (this->_container[n]);
+			};
+        // Returns a reference to the element at position n in the vector. throws out_of_range exeption if out of range
+            reference at(size_type n)
+			{
+				if (n >= _container_length || n < 0)
+					throw (std::out_of_range());
+				return (this->_container[n]);
+			};
+        // Returns a constant reference to the element at position n in the vector. throws out_of_range exeption if out of range
+			const_reference at(size_type n) const
+			{
+				if (n >= _container_length || n < 0)
+					throw (std::out_of_range());
+				return (this->_container[n]);
+			};
+        // Returns a reference to the first element in the vector.
+            reference front(void)
+			{
+				return (this->_container[0]);
+			};
+        // Returns a constant reference to the first element in the vector.
+			const_reference front(void) const
+			{
+				return (this->_container[0]);
+			};
+        // Returns a reference to the last element in the vector.
+            reference back(void)
+			{
+				return (this->_container[this->_length - 1]);
+			};
+        // Returns a constant reference to the last element in the vector.
+			const_reference back(void) const
+			{
+				return (this->_container[this->_length - 1]);
+			};
+        // Assigns new contents to the vector, replacing its current contents, and modifying its size accordingly.
+        // Range : The new contents are elements constructed from each of the elements in the range between first and last, in the same order.
+            template <class InputIterator>
+            void assign(InputIterator first, InputIterator last)
+			{
+				this->clear();
+				this->insert(this->begin(), first, last);
+			};
+        // Fill : The new contents are n elements, each initialized to a copy of val.
+			void assign(size_type n, const value_type &value)
+			{
+				this->clear();
+				this->insert(this->begin(), n, value);
+			};
+        // Adds a new element at the end of the vector, after its current last element. The content of val is copied (or moved) to the new element.
+            void push_back(const value_type &value)
+			{
+				if (this->_length + 1 > this->_size)
+					this->reserve(this->_length + 1);
+				this->_container[this->_length++] = value;
+			};
+        // Removes the last element in the vector, effectively reducing the container size by one.
+            void pop_back(void)
+			{
+				if (this->_length)
+					this->_length--;
+			};
+        // The vector is extended by inserting new elements before the element at the specified position, 
+        // effectively increasing the container size by the number of elements inserted.
+        // Single element
+            iterator insert(iterator position, const value_type &value)
+			{
+				size_type i = 0;
+				iterator it = begin();
+				while (it + i != position && i < this->_length)
+					i++;
+				if (this->_size < this->_length + 1)
+					reserve(this->_length + 1);
+				size_type j = this->_size - 1;
+				while (j > i)
+				{
+					this->_container[j] = this->_container[j - 1];
+					j--;
+				}
+				this->_container[i] = value;
+				this->_length++;
+				return (iterator(&this->_container[i]));
+			};
+        // Fill version
+            void insert(iterator position, size_type n, const value_type &value)
+			{
+				while (n--)
+					position = this->insert(position, value);
+			};
+        // Range version
+			template <class InputIterator>
+			void insert(iterator position, InputIterator begin, InputIterator end)
+			{
+				while (begin != end)
+				{
+					position = this->insert(position, *begin) + 1;
+					++begin;
+				}
+			};
+        // Removes from the vector either a single element (position) or a range of elements ([first,last)).
+        // Single element
+            iterator erase(iterator position)
+			{
+				iterator cursor = position;
+				while (cursor + 1 != end())
+				{
+					*cursor = *(cursor + 1);
+					cursor++;
+				}
+				this->_length--;
+				return (iterator(position));
+			};
+        // Range of elements
+			iterator erase(iterator begin, iterator end)
+			{
+				while (begin != end)
+				{
+					erase(begin);
+					end--;
+				}
+				return (iterator(begin));
+			};
+        // Exchanges the content of the container by the content of x, which is another vector object of the same type. Sizes may differ.
+            void swap (vector& x)
+            {
+                vector<value_type> tmp(this);
+
+                this = x;
+                x = tmp;
+            };
+        // Removes all elements from the vector (which are destroyed), leaving the container with a size of 0.
+            void clear(void)
+			{
+				this->erase(this->begin(), this->end());
+			};
+        // Returns a copy of the allocator object associated with the vector.
+            allocator_type get_allocator() const
+            {
+                return (this->_allocator);
+            };
+    };
+// Non member fonction
+// Boolean operator
+    template<class T, class Alloc>
+    bool operator==(const Vector<T, Alloc> &a, const Vector<T, Alloc> &b)
+	{
+		if (a.size() != b.size())
+			return (false);
+		unsigned long i = -1;
+		while (++i < a.size())
+		{
+			if (a[i] != b[i])
+				return (false);
+		}
+		return (true);
+	};
+	template<class T, class Alloc>
+	bool operator!=(const Vector<T, Alloc> &a, const Vector<T, Alloc> &b)
+	{
+		return (!(a == b));
+	};
+	template <class T, class Alloc>
+	bool operator<(const Vector<T,Alloc> &a, const Vector<T,Alloc> &b)
+	{
+		size_t		i;
+		size_t		n;
+
+		if (a.size() > b.size())
+			n = b.size();
+		else
+			n = a.size();
+		i = 0;
+		while (i < n)
+		{
+			if (a.at(i) != b.at(i))
+				return (a.at(i) < b.at(i));
+			i++;
+		}
+		return (a.size() < b.size());
+	}
+	template <class T, class Alloc>
+	bool operator<=(const Vector<T,Alloc> &a, const Vector<T,Alloc> &b)
+	{
+		return (a < b || a == b);
+	}
+
+	template <class T, class Alloc>
+	bool operator>(const Vector<T,Alloc> &a, const Vector<T,Alloc> &b)
+	{
+		return (!(a < b) && !(a == b));
+	}
+	template <class T, class Alloc>
+	bool operator>=(const Vector<T,Alloc> &a, const Vector<T,Alloc> &b)
+	{
+		return (!(a < b));
+	}
+// Non member fonction swap
+    template <class T, class Alloc>
+    void swap (vector<T,Alloc>& x, vector<T,Alloc>& y)
+    {
+        x.swap(y);
     };
 };
 
